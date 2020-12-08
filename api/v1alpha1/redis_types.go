f4953bae39311d73f59c21e5a60d39a5bc323963
Daniel Dorado <dadorado@atsistemas.com>
2020-12-09 00:46:01 +0100

    Define the API. Redis Types.

--- api/v1alpha1/redis_types.go
+++ api/v1alpha1/redis_types.go
@@ -25,17 +25,16 @@ import (
 
 // RedisSpec defines the desired state of Redis
 type RedisSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Redis. Edit Redis_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Size int32 `json:"size"`
 }
 
 // RedisStatus defines the observed state of Redis
 type RedisStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// Nodes are the names of the redis pods
+	Nodes []string `json:"nodes"`
 }
 
 // +kubebuilder:object:root=true
