f3d4c92cf2c1ef1335a0860fd0a5cc706dfdcbe3
Daniel Dorado <dadorado@atsistemas.com>
2020-12-12 09:00:05 +0100

    First controller.

--- controllers/redis_controller.go
+++ controllers/redis_controller.go
@@ -18,13 +18,27 @@ package controllers
 
 import (
 	"context"
+	"reflect"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/danieldorado/redis-operator/api/v1alpha1"
 	redisv1alpha1 "github.com/danieldorado/redis-operator/api/v1alpha1"
+)
+
+const (
+	// TODO Change this
+	imageRedisOperator = "danieldorado/redisop"
+	imageRedis         = "danieldorado/redis"
 )
 
 // RedisReconciler reconciles a Redis object
@@ -37,11 +51,90 @@ type RedisReconciler struct {
 // +kubebuilder:rbac:groups=redis.danieldorado.github.io,resources=redis,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=redis.danieldorado.github.io,resources=redis/status,verbs=get;update;patch
 
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// Creates or reconciles a StatefulSet, the redis cluster) and an StatefulSet (the redis operator).
 func (r *RedisReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("redis", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("redis", req.NamespacedName)
 
-	// your logic here
+	// Fetch the redis instance
+	redis := &redisv1alpha1.Redis{}
+	err := r.Get(ctx, req.NamespacedName, redis)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use
+			// final
+
+			// Return and don't requeue
+			log.Info("Redis resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		//  Error reading the object - requeue the request.
+		log.Error(err, "Failed to get Redis - reque the request.")
+		return ctrl.Result{}, err
+	}
+	// Check if the StatefulSet already exists, if not create a new one
+	found := &appsv1.StatefulSet{}
+	err = r.Get(ctx, types.NamespacedName{Name: redis.Name, Namespace: redis.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new StatefulSet
+		dep := r.statefulSetForRedis(redis)
+		log.Info("Creating a new StatefulSet", "StatefulSet.Namespace", dep.Namespace, "StatefulSet.Name", dep.Name)
+		err = r.Create(ctx, dep)
+		if err != nil {
+			log.Error(err, "Failed to create new StatefulSet", "StatefulSet.Namespace", dep.Namespace, "StatefulSet.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+		// StatefulSet created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get StatefulSet")
+		return ctrl.Result{}, err
+	}
+	// TODO Create statefulset, how to do this according to the statefulSet??
+	// Check if the statefulset already exsits, if not create a new one
+
+	// There are Both objects
+
+	// TODO Ensure the statefulset size is the same as the spec
+	size := redis.Spec.Size
+
+	log.Info("Redis size", "Redis.Size", size, "StatefulSet.Namespace", redis.Namespace, "StatefulSet.Name", redis.Name)
+	/*
+		if *found.Spec.Replicas != size {
+			found.Spec.Replicas = &size
+			err = r.Update(ctx, found)
+			if err != nil {
+				log.Error(err, "Failed to update StatefulSet", "StatefulSet.Namespace", found.Namespace, "StatefulSet.Name", found.Name)
+				return ctrl.Result{}, err
+			}
+			// Spec updated - return and requeue
+			return ctrl.Result{Requeue: true}, nil
+		}
+	*/
+	// Update the Redis status with the pod names
+	podList := &corev1.PodList{}
+	listOpts := []client.ListOption{
+		client.InNamespace(redis.Namespace),
+		client.MatchingLabels(labelsForRedis(redis.Name)),
+	}
+	if err = r.List(ctx, podList, listOpts...); err != nil {
+		log.Error(err, "Failed to list pods", "Redis.Namespace", redis.Namespace, "Redis.Name", redis.Name)
+		return ctrl.Result{}, err
+	}
+	podNames := getPodNames(podList.Items)
+
+	// Update status.Nodes if needed
+	if !reflect.DeepEqual(podNames, redis.Status.Nodes) {
+		redis.Status.Nodes = podNames
+		err := r.Status().Update(ctx, redis)
+		if err != nil {
+			log.Error(err, "Failed to update Memcached status")
+			return ctrl.Result{}, err
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -50,4 +143,52 @@ func (r *RedisReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&redisv1alpha1.Redis{}).
 		Complete(r)
+}
+
+// TODO fill this
+// statefulSetForRedis returns the redis operator statefulSet which does the cluster operations.
+func (r *RedisReconciler) statefulSetForRedis(redis *v1alpha1.Redis) *appsv1.StatefulSet {
+	ls := labelsForRedis(redis.Name)
+	replicas := int32(redis.Spec.Size)
+
+	dep := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      redis.Name,
+			Namespace: redis.Namespace,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image:   "",
+						Name:    "redisop",
+						Command: []string{},
+						Ports:   []corev1.ContainerPort{},
+					}},
+				}},
+		},
+	}
+	ctrl.SetControllerReference(redis, dep, r.Scheme)
+	return dep
+}
+
+// TODO fill this
+func labelsForRedis(name string) map[string]string {
+	return map[string]string{"app": "redis", "redis_cr": name}
+}
+
+// TODO fill this
+func getPodNames(pods []corev1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
 }
